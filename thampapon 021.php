994a1e5af7f490379a6107aed436b9e555b626e4
Thampapon <othrrmpphl391@gmail.com>
2021-01-15 16:45:05 +0700

    edit thampapon

---
+++ thampapon 021.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>ธรรมปพน โคกเทียน</h2>
+    <h2>นักเรียน</h2>
+</body>
+</html>
